ab15e4d43b2215a54bc4b699f023559d7d283e2e
Guillaume RICHARD <g.jf.richard@gmail.com>
2015-07-22 09:39:56 +0200

    ajout des commentaires

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-// inspiration : http://www.grafikart.fr/tutoriels/php/router-628
-
 // gestion des erreurs
 ini_set('display_errors','on');
 error_reporting(E_ALL);
 
+// on appel l'autoloading
 require "vendor/autoload.php";
 
+// on créé un objet $router
 $router = new App\Router\Router($_GET['url']);
 
+// exemple de route créé par le router
 $router->get("/", function() { echo "Bonjour"; });
 $router->get("/posts", function() { echo "Tous les articles"; });
 
 $router->get(
 	"/posts/:slug-:id",
 	function($slug, $id) use ($router) {
-		// echo "Article $slug / $id";
 		echo $router->url('post.show', ['id' => 1, 'slug' => 'salut-les-gens']);
 	},
 	'post.show'
@@ -26,4 +26,5 @@ $router->get(
 $router->get("/posts/:id", "Posts#show");
 $router->post("/posts/:id", function($id) { echo "Poster pour l'article ".$id; });
 
+// on démarre l'application
 $router->run();
